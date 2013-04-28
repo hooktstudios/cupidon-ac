43ec0a049f491fed6ff8fa7e9b9483440e0b7938
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-04-28 18:11:36 -0400

    Update grunt usage, dependencies & test script

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,11 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         qunit: {
-            files: ['test.html']
+          files: ['test.html']
         }
     });
 
     // Task to run tests
+    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.registerTask('test', 'qunit');
 };
